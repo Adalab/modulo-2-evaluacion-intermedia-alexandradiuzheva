faca636134abe2e6156199ebd77b516e8bc186b0
alexandradiuzheva <sandra-d@inbox.ru>
2022-07-06 11:03:44 +0200

    Add condition

--- js/main.js
+++ js/main.js
@@ -10,11 +10,28 @@ const attempt = document.querySelector('.js-attempt');
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
   }
+
 console.log(getRandomNumber(100));
 
 function handleClick(ev) {
   ev.preventDefault();
   console.log (number.value);
+
+  const check = parseInt(number.value);
+
+if (check === getRandomNumber(100)) {
+  advice.value='Has ganado campeona!!!'
+}
+
+if (check < getRandomNumber(100)) {
+  advice.value='Demasiado bajo';
+} else {
+  advice.value='Demasiado alto';
+}
+
 }
 
 btn.addEventListener('click', handleClick);
+
+
+
