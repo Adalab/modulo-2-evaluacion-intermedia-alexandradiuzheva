617c46d687a5adda0b6fff7fc7cfe1482a38b951
alexandradiuzheva <sandra-d@inbox.ru>
2022-07-10 18:19:16 +0200

    Exercize finished

--- js/main.js
+++ js/main.js
@@ -1,37 +1,54 @@
-'use strict';
+"use strict";
 
-const main = document.querySelector('.js-main');
-const number = document.querySelector('.js-number');
-const btn = document.querySelector('.js-btn');
-const advice = document.querySelector('.js-advice');
-const attempt = document.querySelector('.js-attempt');
+// ELEMENTOS DEL HTML
 
+const userNumberInput = document.querySelector(".js-userNumber");
+const tipsElement = document.querySelector(".js-tips");
+const btn = document.querySelector(".js-btn");
+const attempt = document.querySelector(".js-attempt");
+
+// FUNCIONES
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
+}
+
+function checkNumbers () {
+  const userNumber = parseInt(userNumberInput.value);
+
+  if (isNaN(userNumber) || userNumber > 100 || userNumber < 1) {
+    tipsElement.value = 'El número debe estar entre 1 y 100';
   }
-
-console.log(getRandomNumber(100));
-
-function handleClick(ev) {
-  ev.preventDefault();
-  console.log (number.value);
-
-  const check = parseInt(number.value);
-
-if (check === getRandomNumber(100)) {
-  advice.value='Has ganado campeona!!!'
+  else if (userNumber > randomNumber) {
+    tipsElement.value = 'Demasiado alto';
+  }
+  else if (userNumber < randomNumber) {
+    tipsElement.value = 'Demasiado bajo';
+  }
+  else if (userNumber === randomNumber) {
+    tipsElement.value = 'Has ganado campeona!!!';
+  }
 }
 
-if (check < getRandomNumber(100)) {
-  advice.value='Demasiado bajo';
-} else {
-  advice.value='Demasiado alto';
+function incrementCounter () {
+  attemptCounter++;
+  attempt.value = attemptCounter;
 }
 
+function handleClick(event) {
+  event.preventDefault();
+
+  checkNumbers ();
+  
+  incrementCounter ();  
 }
+
+// EVENTOS 
 
 btn.addEventListener('click', handleClick);
 
+// CÓDIGO QUE SE EJECUTA CUANDO CARGA LA PÁGINA (MAIN/GLOBAL)
 
-
+let attemptCounter = 0;
+const randomNumber = getRandomNumber( 100 );
+console.log(`Mi número aleatorio es ${randomNumber}`);
